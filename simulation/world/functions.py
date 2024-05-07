5ea4dba40e1724dfd822b4a1d195f12007d93345
ZagerMarvin <105629688+ZagerMarvin@users.noreply.github.com>
2024-05-07 13:14:48 +0200

    Update functions.py

--- simulation/world/functions.py
+++ simulation/world/functions.py
@@ -24,31 +24,3 @@ def generate_random_start_positions(num_positions, obstacles, WIDTH, HEIGHT, INI
     
     start_positions_dict = {i+1: pos for i, pos in enumerate(start_positions)}
     return start_positions_dict
-
-def generate_random_target_position(obstacles, WIDTH, HEIGHT, min_distance=15):
-    obstacle_polygons = [Polygon(obstacle) for obstacle in obstacles]
-
-    while True:
-        x = random.randint(0, WIDTH)
-        y = random.randint(0, HEIGHT)
-        
-        point = Point(x, y)
-        if all(point.distance(obstacle) >= min_distance for obstacle in obstacle_polygons):
-            return x, y
-        
-def generate_random_target_positions(num_positions, obstacles, WIDTH, HEIGHT, min_distance=15):
-    target_positions = []
-    obstacle_polygons = [Polygon(obstacle) for obstacle in obstacles]
-
-    while len(target_positions) < num_positions:
-        # generate random coordinates
-        x = random.randint(0, WIDTH)
-        y = random.randint(0, HEIGHT)
-        
-        # check whether the generated point is far enough away from all obstacles
-        point = Point(x, y)
-        if all(point.distance(obstacle) >= min_distance for obstacle in obstacle_polygons):
-            target_positions.append((x, y))
-    
-    target_positions_dict = {i+1: pos for i, pos in enumerate(target_positions)}
-    return target_positions_dict
